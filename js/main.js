5fa21d0afd7b5fe1a0a7d3b3fabbc905779abaab
PlaySimon <semenplyaskin@gmail.com>
2019-10-18 20:14:46 +0200

    Lessons-01

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let money;
+const income = '';
+let addExpenses;
+let deposit;
+let mission;
+const period = '';
+
+alert( "Привет" );
+alert( income );
+
+console.log(deposit);
+console.log('deposit');
